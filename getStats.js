42db9ccc277b04b92e9ca3d8183f6c97e1012a7b
rcscrewX <ramonsenger282@outlook.com>
2023-03-13 02:21:20 +0000

    GETSTATS - comentarios

--- getStats.js
+++ getStats.js
@@ -1,12 +1,12 @@
-const os = require('os');
-const moment = require('moment');
+const os = require('os');          // puxar o freemem, totalmem.
+const moment = require('moment');  // puxar a data.
 
 function getStats() {
   const { freemem, totalmem } = os;
 
-  let mem = parseInt(freemem() / 1024 / 1024);
+  let mem = parseInt(freemem() / 1024 / 1024);   // dividi 2x por 1024, para chegar em MB. por causa que esta em bit.
   let total = parseInt(totalmem() / 1024 / 1024);
-  let porcents = parseInt((mem / total) * 100);
+  let porcents = parseInt((mem / total) * 100);   // 100x para chegar em %
 
   let stats = {
     free: `${mem} MB`,
