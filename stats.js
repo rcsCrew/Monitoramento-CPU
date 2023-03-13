7b25e1a7b5fc57c66c67cdde750f57febe89244c
rcscrewX <ramonsenger282@outlook.com>
2023-03-13 00:48:15 +0000

    fix

--- stats.js
+++ stats.js
@@ -1,23 +1,36 @@
 const { stat } = require('fs');
 const os = require('os');
+const log = require('./logger');
+const moment = require('moment');
 
 let i = 1
 
+
+function getStats() {
+  const { freemem, totalmem } = os
+
+  let mem = parseInt(freemem() / 1024 / 1024)
+  let total = parseInt(totalmem() / 1024 / 1024)
+  let porcents = parseInt((mem / total) * 100)
+  
+  let stats = {
+      free: `${mem} MB`,
+      total: `${total} MB`,
+      usage: `${porcents}%`,
+  }
+  
+  let output = `===== PC STATS ${i} =====\n`
+  output += `free : ${stats.free}\n`
+  output += `total: ${stats.total}\n`
+  output += `usage: ${stats.usage}\n`
+  output += `time : ${moment().format('DD/MM/YYYY HH:mm:ss')}\n`
+  
+  return output
+};
+
+
 setInterval(() => {
-    const { freemem, totalmem } = os
-
-    let mem = parseInt(freemem() / 1024 / 1024)
-    let total = parseInt(totalmem() / 1024 / 1024)
-    let porcents = parseInt((mem / total) * 100)
-    
-    let stats = {
-        free: `${mem} MB`,
-        total: `${total} MB`,
-        usage: `${porcents}%`,
-    }
-    
-    console.info(`===== PC STATS ${i} =====`)
-    console.table(stats)
-    i += 1
-}, 3000);
-
+  const stats = getStats()
+  log(stats)
+  i += 1
+}, 5000);
